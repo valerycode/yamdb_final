e0587e154fd0dffd27e15da9fb5df282a4ae6e98
valerycode <lera200@yandex.ru>
2021-06-08 22:53:25 +0300

    Change file backends.py

--- api/backends.py
+++ api/backends.py
@@ -6,9 +6,10 @@ from .models.user_confirm_code_db import UserConfirmCodeDb
 class ConfirmUserRegistrationAuthentication(authentication.BaseAuthentication):
     """Аутентификация будущего пользователя."""
     def authenticate(self, request, email=None, confirm_code=None):
+        user = UserConfirmCodeDb.objects.get(
+            email=email, confirm_code=confirm_code)
         try:
-            user = UserConfirmCodeDb.objects.get(email=email,
-                                                 confirm_code=confirm_code)
+            user
         except UserConfirmCodeDb.DoesNotExist:
             return None
         return user
